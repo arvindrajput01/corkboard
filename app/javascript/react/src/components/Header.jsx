460c1b4fecac44ed7a85a4075bec5744d85fc848
Arvind Kumar <arvindrajput2010@gmail.com>
2022-09-16 02:20:06 +1000

    css changes

--- app/javascript/react/src/components/Header.jsx
+++ app/javascript/react/src/components/Header.jsx
@@ -3,14 +3,40 @@ import { NavLink } from "react-router-dom";
 import { Navbar,NavDropdown,Form,FormControl,Button,Dropdown } from 'react-bootstrap'
 
 import styled from 'styled-components'
-import { slide as Menu } from 'react-burger-menu';
+import { bubble as Menu } from 'react-burger-menu'; //we can use slide instead of bubble
 import '../stylesheet/Sidebar.css';
+
+const Wrapper = styled.nav`
+  width: 100%;
+  height: 65px;
+  line-height: 65px;
+  background-color: #fffff;
+  color: white;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 100%;
+  border-bottom: 1px solid rgb(205, 209, 210);
+  border-top: 1px solid rgb(25,121,169);
+  border-top-width:3px;
+  border-bottom-width:thin;
+  position: fixed;
+
+
+`
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 100%;
+`
 
 function Header() {
 
 
 
   return (
+    <Wrapper>
+    <Container>
     <Menu>
       <a className="menu-item" href="/">
         Home
@@ -25,6 +51,8 @@ function Header() {
         Desserts
       </a>
     </Menu>
+    </Container>
+    </Wrapper>
     
 
 
